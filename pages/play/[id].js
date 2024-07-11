a261ebb4178394ab21d8effd5774d5a4ee8a556c
sh20raj <shaswatraj3@gmail.com>
2024-07-11 23:30:16 +0530

    fullscreen

--- pages/play/[id].js
+++ pages/play/[id].js
@@ -11,7 +11,7 @@ export default function Id() {
       <h1>Watch Movie</h1>
       {/* <iframe src={'https://vidsrc.xyz/embed/'+id} width="100%" height="100%" frameBorder="0" scrolling="no" allowFullScreen></iframe> */}
 
-      <iframe width="800px" height="515" src={'https://vidsrc.xyz/embed/'+id} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe width="800px" height="515" src={'https://vidsrc.xyz/embed/'+id} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; fullscreen; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowFullScreen={true}></iframe>
     </div>
   )
 }
